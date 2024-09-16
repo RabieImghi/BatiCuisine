a72d0c2764455f4a7ada4a690441e03a0fd48319
RabieImghi <rabieaitimghi7@gmail.com>
2024-09-16 11:38:09 +0100

    creatr Matreial Component extent Compennt and create save Function

--- src/repository/MaterialRepository.java
+++ src/repository/MaterialRepository.java
@@ -1,32 +1,37 @@
 package repository;
 
 import config.DatabaseConnection;
-import domain.Component;
+import domain.Material;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.util.Optional;
 
-public class ComponentRepository {
+public class MaterialRepository {
     private final Connection connection;
-    public ComponentRepository() {
+
+    public MaterialRepository(){
         this.connection = DatabaseConnection.getInstance().getConnection();
     }
 
-    public Optional<Component> save(Component component){
+    public Optional<Material> save(Material material){
         try {
             this.connection.setAutoCommit(false);
-            String stmt = "INSERT INTO components (name, component_type, vat_rate, project_id) VALUES (?,?,?,?)";
+            String stmt = "INSERT INTO materials (name, component_type, vat_rate, project_id, unit_cost, quantity, transport_cost, quality_coefficient) VALUES (?,?,?,?,?,?,?,?)";
             PreparedStatement saveStmt = this.connection.prepareStatement(stmt, PreparedStatement.RETURN_GENERATED_KEYS );
-            saveStmt.setString(1, component.getName());
-            saveStmt.setString(2, component.getComponentType());
-            saveStmt.setDouble(3, component.getVatRate());
-            saveStmt.setInt(4, component.getProject().getId());
+            saveStmt.setString(1, material.getName());
+            saveStmt.setString(2, material.getComponentType());
+            saveStmt.setDouble(3, material.getVatRate());
+            saveStmt.setInt(4, material.getProject().getId());
+            saveStmt.setDouble(5, material.getUnitCost());
+            saveStmt.setDouble(6, material.getQuantity());
+            saveStmt.setDouble(7, material.getTransportCost());
+            saveStmt.setDouble(8, material.getQualityCoefficient());
             int results = saveStmt.executeUpdate();
             if(results > 0){
                 int id = saveStmt.getGeneratedKeys().getInt(1);
-                component.setId(id);
-                return Optional.of(component);
+                material.setId(id);
+                return Optional.of(material);
             }
         }catch (Exception e){
             try {
--- src/service/impl/ComponentServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package service.impl;
-
-import domain.Component;
-
-import java.util.Optional;
-
-public interface ComponentServiceImpl {
-    public Optional<Component> save(Component component);
-}
